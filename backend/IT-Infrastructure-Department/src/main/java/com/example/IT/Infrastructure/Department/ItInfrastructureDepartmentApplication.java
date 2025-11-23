1006fe08291f5ae264dc52f45d7d30af128fec07
baothanh4 <taobaothanh365@gmail.com>
2025-11-23 14:49:02 +0700

    Add backend and frontend structure

---
+++ backend/IT-Infrastructure-Department/src/main/java/com/example/IT/Infrastructure/Department/ItInfrastructureDepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.IT.Infrastructure.Department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItInfrastructureDepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItInfrastructureDepartmentApplication.class, args);
+	}
+
+}
---
+++ backend/IT-Infrastructure-Department/src/test/java/com/example/IT/Infrastructure/Department/ItInfrastructureDepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IT.Infrastructure.Department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItInfrastructureDepartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
